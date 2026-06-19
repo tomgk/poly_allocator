f3ef479f1a93b7b66ca962f4c5081b33cad9f240
T. G. <6294097+tomgk@users.noreply.github.com>
2026-06-20 01:36:57 +0200

    introduce googletest

--- arena_allocator_test.cpp
+++ arena_allocator_test.cpp
@@ -1,4 +1,41 @@
 #include "arena_allocator.hpp"
+#include <gtest/gtest.h>
+
+TEST(HelloTest, BasicAssertions)
+{
+    ArenaAllocator<true> a;
+
+    auto ptr = a.allocate<int>(12);
+    a.deallocate(ptr);
+    a.get_type_info(ptr);
+    std::cout << "capacity: " << a.get_capacity() << std::endl;
+    a.get_used_bytes();
+    a.clear();
+
+    for(int i=0;i<256;++i)
+    {
+        a.allocate<std::string>("#"+std::to_string(i));
+    }
+
+    for(auto iter=a.begin();iter!=a.end();++iter)
+    {
+        const std::type_info &type = *iter.get_header().type_info;
+        std::cout << type.name();
+
+        if(typeid(std::string) == type)
+        {
+            std::cout << "STR " << iter.get<std::string>();
+        }
+        else if(typeid(std::string) == type)
+        {
+            std::cout << "INT " << iter.get<int>();
+        }
+
+        std::cout << std::endl;
+    }
+
+    std::cout << "capacity: " << a.get_capacity() << std::endl;
+}
 
 void test()
 {
@@ -10,11 +47,6 @@ void test()
     a.get_capacity();
     a.get_used_bytes();
     a.clear();
-
-    for(void *c : a)
-    {
-
-    }
 
     for(auto iter = a.begin();iter != a.end(); ++iter)
     {
