c73bc56b8691f5bb0a9770b98c783637f26035f5
T. G. <6294097+tomgk@users.noreply.github.com>
2026-06-22 21:15:49 +0200

    add disabled test that doesn't work

--- arena_allocator_test.cpp
+++ arena_allocator_test.cpp
@@ -15,8 +15,34 @@ TEST(ArenaAllocator, OneTypeWithoutResizeAndClear_Complex_Once)
 {
     ArenaAllocator<true> a;
 
+    a.allocate<std::string>("#0");
+
+    for(auto iter=a.begin();iter!=a.end();++iter)
+    {
+        const std::type_info &type = *iter.get_header().type_info;
+        std::cout << type.name();
+
+        if(typeid(std::string) == type)
+        {
+            std::cout << "STR " << iter.get<std::string>();
+        }
+        else if(typeid(std::string) == type)
+        {
+            std::cout << "INT " << iter.get<int>();
+        }
+
+        std::cout << std::endl;
+    }
+
+    std::cout << "capacity: " << a.get_capacity() << std::endl;
+}
+
+TEST(ArenaAllocator, DISABLED_OneTypeWithoutResizeAndClear_Complex_MultipleFull)
+{
+    ArenaAllocator<true> a;
+
     //TODO: i<8 (which requires reallocation) causes a crash for some reason
-    for(int i=0;i<1/*256*/;++i)
+    for(int i=0;i<7;++i)
     {
         a.allocate<std::string>("#"+std::to_string(i));
         std::cout << i << std::endl;
