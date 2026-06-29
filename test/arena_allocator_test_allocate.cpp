9ba78ddffddf91ce94a25bb377aa6676127052f9
T. G. <6294097+tomgk@users.noreply.github.com>
2026-06-29 20:22:45 +0200

    split up tests

---
+++ test/arena_allocator_test_allocate.cpp
@@ -0,0 +1,30 @@
+#define ARENA_ALLOCATOR_LOG
+#include "arena_allocator.hpp"
+
+#include <gtest/gtest.h>
+#include "common.h"
+
+#include "data.h"
+
+TEST(ArenaAllocator_allocate, OneTypeWithoutResizeAndClear_Complex_Once)
+{
+    TypeAwareArenaAllocator a;
+
+    a.allocate<std::string>("#0");
+
+    dump(a);
+}
+
+TEST(ArenaAllocator_allocate, DISABLED_OneTypeWithoutResizeAndClear_Complex_MultipleFull)
+{
+    TypeAwareArenaAllocator a;
+
+    //TODO: i<8 (which requires reallocation) causes a crash for some reason
+    for(int i=0;i<7;++i)
+    {
+        a.allocate<std::string>("#"+std::to_string(i));
+        std::cout << i << std::endl;
+    }
+
+    dump(a);
+}
