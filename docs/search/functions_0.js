ebb224e41af2230e51b8628c4540997267749ca6
GitHub Action <action@github.com>
2026-07-13 19:12:10 +0000

    docs: Update documentation from main branch

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['allocate_0',['allocate',['../classArenaAllocator.html#a1011c02ffa8636083de4c09ee97fb5a6',1,'ArenaAllocator']]],
-  ['allocatearray_1',['allocatearray',['../classArenaAllocator.html#a15c440cb45186178255b392f836a0b1a',1,'ArenaAllocator::allocateArray(std::size_t count, bool zero_initialize=false)'],['../classArenaAllocator.html#a98e7b7baf0da2b73981f20c3ef370238',1,'ArenaAllocator::allocateArray(std::size_t count, const T &amp;value)']]],
+  ['allocatearray_1',['allocatearray',['../classArenaAllocator.html#a046d08b0950e02a532194a98c3c7aa77',1,'ArenaAllocator::allocateArray(std::size_t count, bool zero_initialize=false)'],['../classArenaAllocator.html#ae9008faa21b78496f09baffa02405579',1,'ArenaAllocator::allocateArray(std::size_t count, const T &amp;value)']]],
   ['allocateraw_2',['allocateRaw',['../classArenaAllocator.html#aca7250a5e890971bb78b46a2444a95a3',1,'ArenaAllocator']]],
   ['allocatewithnooffset_3',['allocateWithNoOffset',['../classArenaAllocator.html#abbd696e0be51530d77fbb7e82c1511f7',1,'ArenaAllocator']]],
   ['arenaallocator_4',['ArenaAllocator',['../classArenaAllocator.html#a30331f7e068005dc0b7a6f02f6c9f967',1,'ArenaAllocator']]]
