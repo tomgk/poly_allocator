8685ffdf3a3f13c037a7c86d1390efe113d8e7c1
GitHub Action <action@github.com>
2026-07-16 19:01:56 +0000

    docs: Update documentation from main branch

--- docs/classArenaAllocator_1_1IteratorImpl.js
+++ docs/classArenaAllocator_1_1IteratorImpl.js
@@ -2,7 +2,7 @@ var classArenaAllocator_1_1IteratorImpl =
 [
     [ "get_header", "classArenaAllocator_1_1IteratorImpl.html#a165ab313c0e2355c5568ab5622251f5e", null ],
     [ "operator!=", "classArenaAllocator_1_1IteratorImpl.html#a518fdb3102fec97a66ae25df6288c742", null ],
-    [ "operator*", "classArenaAllocator_1_1IteratorImpl.html#af2cf417c2c713963290edd646f6c382c", null ],
+    [ "operator*", "classArenaAllocator_1_1IteratorImpl.html#a3c1fc11cb3f549dbaf4483599a21a9ce", null ],
     [ "operator++", "classArenaAllocator_1_1IteratorImpl.html#a378c127257f3750a3f57efec8a128941", null ],
     [ "operator++", "classArenaAllocator_1_1IteratorImpl.html#a52932cea9bf97e87904f04a9f8ff5501", null ],
     [ "operator==", "classArenaAllocator_1_1IteratorImpl.html#a5be662015e48ead1c7ad51544b6790b4", null ]
--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['on_5freallocation_0',['on_reallocation',['../classArenaAllocator.html#aa2af9018fa56a6cfc20cdebefd0815a4',1,'ArenaAllocator']]],
   ['operator_21_3d_1',['operator!=',['../classArenaAllocator_1_1IteratorImpl.html#a518fdb3102fec97a66ae25df6288c742',1,'ArenaAllocator::IteratorImpl']]],
-  ['operator_2a_2',['operator*',['../classArenaAllocator_1_1IteratorImpl.html#af2cf417c2c713963290edd646f6c382c',1,'ArenaAllocator::IteratorImpl']]],
+  ['operator_2a_2',['operator*',['../classArenaAllocator_1_1IteratorImpl.html#a3c1fc11cb3f549dbaf4483599a21a9ce',1,'ArenaAllocator::IteratorImpl']]],
   ['operator_2b_2b_3',['operator++',['../classArenaAllocator_1_1IteratorImpl.html#a378c127257f3750a3f57efec8a128941',1,'ArenaAllocator::IteratorImpl::operator++()'],['../classArenaAllocator_1_1IteratorImpl.html#a52932cea9bf97e87904f04a9f8ff5501',1,'ArenaAllocator::IteratorImpl::operator++(int)']]],
   ['operator_3d_3d_4',['operator==',['../classArenaAllocator_1_1IteratorImpl.html#a5be662015e48ead1c7ad51544b6790b4',1,'ArenaAllocator::IteratorImpl']]]
 ];
