1b81eb8939ca4157e861e24a954ba7a7928c15f8
T. G. <6294097+tomgk@users.noreply.github.com>
2026-07-21 21:06:27 +0200

    move Callback_CopyArray_Independent to different file

--- lib/arena_allocator.hpp
+++ lib/arena_allocator.hpp
@@ -10,6 +10,7 @@
 
 #include "arena_allocator_basic.h"
 #include "callback.h"
+#include "callback_array.h"
 
 // Forward declaration of the allocator
 template <ArenaMode M>
@@ -849,50 +850,6 @@ public:
 
 
 private:
-    template<typename T>
-    static void Callback_CopyArray_Independent(void* src, void* dst, std::size_t element_count, std::ptrdiff_t offset)
-    {
-        if (!src || !dst || element_count == 0) return;
-
-        T* src_array = reinterpret_cast<T*>(src);
-        T* dst_array = reinterpret_cast<T*>(dst);
-        std::size_t total_bytes = element_count * sizeof(T);
-
-        // Pfad 1: Für triviale Typen/Plain Objects (memcpy)
-        if constexpr (PlainObject<T>)
-        {
-            std::memcpy(dst, src, total_bytes);
-        }
-        // Pfad 2: Für komplexe Objekte (Placement-New mit Element-Iterierung)
-        else
-        {
-            std::size_t copied = 0;
-            try
-            {
-                for (; copied < element_count; ++copied)
-                {
-                    if constexpr (CopyWithOffsetConstructable<T>)
-                    {
-                        new (&dst_array[copied]) T(copyWithOffsetConstruct, std::move(src_array[copied]), offset);
-                    }
-                    else
-                    {
-                        // Fallback für Typen ohne Custom-Offset-Unterstützung
-                        new (&dst_array[copied]) T(std::move(src_array[copied]));
-                    }
-                }
-            }
-            catch (...)
-            {
-                // Rollback bei Konstruktor-Ausnahme
-                for (std::size_t i = copied; i > 0; --i)
-                {
-                    dst_array[i - 1].~T();
-                }
-                throw;
-            }
-        }
-    }
 
     template<typename T>
     static void Callback_CopyArray(void* src, void* dst, std::ptrdiff_t offset)
