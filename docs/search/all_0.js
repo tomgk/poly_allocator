3c09b6c12b468ba8d16a95f2337b240595e59247
GitHub Action <action@github.com>
2026-07-22 19:01:23 +0000

    docs: Update documentation from main branch

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,12 +1,14 @@
 var searchData=
 [
   ['allocate_0',['allocate',['../classArenaAllocator.html#a410cfc5048f4b44fbe48f7d9eba681b5',1,'ArenaAllocator']]],
-  ['allocatearray_1',['allocatearray',['../classArenaAllocator.html#a6546d9f15c1549fabf7b0ab3c7028c17',1,'ArenaAllocator::allocateArray(std::size_t count, bool zero_initialize=false)'],['../classArenaAllocator.html#adcbf7165623c74e5704c3925cc31ca28',1,'ArenaAllocator::allocateArray(std::size_t count, const T &amp;value)']]],
-  ['allocateraw_2',['allocateRaw',['../classArenaAllocator.html#a6eda4080c85aefa57ec03479908b1b88',1,'ArenaAllocator']]],
-  ['allocatewithnooffset_3',['allocateWithNoOffset',['../classArenaAllocator.html#a6a0185726ed5df195a9a6a744b316d34',1,'ArenaAllocator']]],
-  ['arenaallocator_4',['arenaallocator',['../classArenaAllocator.html',1,'ArenaAllocator&lt; M &gt;'],['../classArenaAllocator.html#a0efe6260d43fae78d06ab91a61fb2115',1,'ArenaAllocator::ArenaAllocator()']]],
-  ['arenaallocator_3c_20m_20_3e_5',['ArenaAllocator&lt; M &gt;',['../classArenaAllocator.html',1,'']]],
-  ['arenaallocatorconstructable_6',['ArenaAllocatorConstructable',['../conceptArenaAllocatorConstructable.html',1,'']]],
-  ['arenaarrayview_7',['ArenaArrayView',['../classArenaArrayView.html',1,'']]],
-  ['arenabufferhandle_8',['ArenaBufferHandle',['../classArenaBufferHandle.html',1,'']]]
+  ['allocatearray_1',['allocateArray',['../classArenaAllocator.html#adcbf7165623c74e5704c3925cc31ca28',1,'ArenaAllocator']]],
+  ['allocatearray1_2',['allocateArray1',['../classArenaAllocator.html#a5d2cac8221df3f65bb12073f01c1a244',1,'ArenaAllocator']]],
+  ['allocatearray2_3',['allocateArray2',['../classArenaAllocator.html#a41cee9e85d22939ac784f01865a18bdc',1,'ArenaAllocator']]],
+  ['allocateraw_4',['allocateRaw',['../classArenaAllocator.html#a6eda4080c85aefa57ec03479908b1b88',1,'ArenaAllocator']]],
+  ['allocatewithnooffset_5',['allocateWithNoOffset',['../classArenaAllocator.html#a6a0185726ed5df195a9a6a744b316d34',1,'ArenaAllocator']]],
+  ['arenaallocator_6',['arenaallocator',['../classArenaAllocator.html',1,'ArenaAllocator&lt; M &gt;'],['../classArenaAllocator.html#a0efe6260d43fae78d06ab91a61fb2115',1,'ArenaAllocator::ArenaAllocator()']]],
+  ['arenaallocator_3c_20m_20_3e_7',['ArenaAllocator&lt; M &gt;',['../classArenaAllocator.html',1,'']]],
+  ['arenaallocatorconstructable_8',['ArenaAllocatorConstructable',['../conceptArenaAllocatorConstructable.html',1,'']]],
+  ['arenaarrayview_9',['ArenaArrayView',['../classArenaArrayView.html',1,'']]],
+  ['arenabufferhandle_10',['ArenaBufferHandle',['../classArenaBufferHandle.html',1,'']]]
 ];
