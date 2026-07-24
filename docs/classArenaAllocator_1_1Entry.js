9e7352f2c2d07048ef58dd8d7bd2c8f5bb763534
GitHub Action <action@github.com>
2026-07-24 19:46:57 +0000

    docs: Update documentation from main branch

--- docs/classArenaAllocator_1_1Entry.js
+++ docs/classArenaAllocator_1_1Entry.js
@@ -1,4 +1,7 @@
 var classArenaAllocator_1_1Entry =
 [
+    [ "get", "classArenaAllocator_1_1Entry.html#a6aee39ee11e57d25153b0870abb2da40", null ],
+    [ "get", "classArenaAllocator_1_1Entry.html#a2b3b9d746b1102bedeea6763da07582b", null ],
+    [ "get_header", "classArenaAllocator_1_1Entry.html#a319ae288b2fd8210fb05216c35a782fc", null ],
     [ "get_size", "classArenaAllocator_1_1Entry.html#ac068b6a29916f77796f2c99a36f87af1", null ]
 ];
--- docs/search/all_6.js
+++ docs/search/all_6.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['get_5fallocation_5fcount_0',['get_allocation_count',['../classArenaAllocator.html#a64432144701bb2aaa8bda12b492a5e12',1,'ArenaAllocator']]],
-  ['get_5fcapacity_1',['get_capacity',['../classArenaAllocator.html#a40a62ad23dfd20c51a994fb47602bbac',1,'ArenaAllocator']]],
-  ['get_5fdead_5fbytes_2',['get_dead_bytes',['../classArenaAllocator.html#a908a68f76611e91abd4eef1493d34ecf',1,'ArenaAllocator']]],
-  ['get_5ffragmentation_5fratio_3',['get_fragmentation_ratio',['../classArenaAllocator.html#a03fba71c658289cf2661895dd805b0d1',1,'ArenaAllocator']]],
-  ['get_5fheader_4',['get_header',['../classArenaAllocator_1_1IteratorImpl.html#a165ab313c0e2355c5568ab5622251f5e',1,'ArenaAllocator::IteratorImpl']]],
-  ['get_5fmax_5fcapacity_5',['get_max_capacity',['../classArenaAllocator.html#a65a0613657676cfbc0f94362d8cc8e74',1,'ArenaAllocator']]],
-  ['get_5fsize_6',['get_size',['../classArenaAllocator_1_1Entry.html#ac068b6a29916f77796f2c99a36f87af1',1,'ArenaAllocator::Entry']]],
-  ['get_5ftotal_5fbytes_5ffor_5ftype_7',['get_total_bytes_for_type',['../classArenaAllocator.html#a286a7c27d0e68a1765dc66a440f362b3',1,'ArenaAllocator']]],
-  ['get_5ftype_5finfo_8',['get_type_info',['../classArenaAllocator.html#a37a5d3a32ff5077cb307e6326e4ea0b8',1,'ArenaAllocator']]],
-  ['get_5fused_5fbytes_9',['get_used_bytes',['../classArenaAllocator.html#aeacac709b8b6592bdaa86cc49ab01363',1,'ArenaAllocator']]],
-  ['getarraycount_10',['getArrayCount',['../classArenaAllocator.html#a5b2bac56526e9399c6ecb83bbc2ec5b9',1,'ArenaAllocator']]]
+  ['get_0',['get',['../classArenaAllocator_1_1Entry.html#a6aee39ee11e57d25153b0870abb2da40',1,'ArenaAllocator::Entry::get()'],['../classArenaAllocator_1_1Entry.html#a2b3b9d746b1102bedeea6763da07582b',1,'ArenaAllocator::Entry::get() const']]],
+  ['get_5fallocation_5fcount_1',['get_allocation_count',['../classArenaAllocator.html#a64432144701bb2aaa8bda12b492a5e12',1,'ArenaAllocator']]],
+  ['get_5fcapacity_2',['get_capacity',['../classArenaAllocator.html#a40a62ad23dfd20c51a994fb47602bbac',1,'ArenaAllocator']]],
+  ['get_5fdead_5fbytes_3',['get_dead_bytes',['../classArenaAllocator.html#a908a68f76611e91abd4eef1493d34ecf',1,'ArenaAllocator']]],
+  ['get_5ffragmentation_5fratio_4',['get_fragmentation_ratio',['../classArenaAllocator.html#a03fba71c658289cf2661895dd805b0d1',1,'ArenaAllocator']]],
+  ['get_5fheader_5',['get_header',['../classArenaAllocator_1_1Entry.html#a319ae288b2fd8210fb05216c35a782fc',1,'ArenaAllocator::Entry::get_header()'],['../classArenaAllocator_1_1IteratorImpl.html#a165ab313c0e2355c5568ab5622251f5e',1,'ArenaAllocator::IteratorImpl::get_header()']]],
+  ['get_5fmax_5fcapacity_6',['get_max_capacity',['../classArenaAllocator.html#a65a0613657676cfbc0f94362d8cc8e74',1,'ArenaAllocator']]],
+  ['get_5fsize_7',['get_size',['../classArenaAllocator_1_1Entry.html#ac068b6a29916f77796f2c99a36f87af1',1,'ArenaAllocator::Entry']]],
+  ['get_5ftotal_5fbytes_5ffor_5ftype_8',['get_total_bytes_for_type',['../classArenaAllocator.html#a286a7c27d0e68a1765dc66a440f362b3',1,'ArenaAllocator']]],
+  ['get_5ftype_5finfo_9',['get_type_info',['../classArenaAllocator.html#a37a5d3a32ff5077cb307e6326e4ea0b8',1,'ArenaAllocator']]],
+  ['get_5fused_5fbytes_10',['get_used_bytes',['../classArenaAllocator.html#aeacac709b8b6592bdaa86cc49ab01363',1,'ArenaAllocator']]],
+  ['getarraycount_11',['getArrayCount',['../classArenaAllocator.html#a5b2bac56526e9399c6ecb83bbc2ec5b9',1,'ArenaAllocator']]]
 ];
