165104614e5031cd109715ee1acd2aaa6f938bdb
T. G. <6294097+tomgk@users.noreply.github.com>
2026-07-27 22:13:45 +0200

    remove support for ArenaArrayView

--- lib/arena_allocator_basic.h
+++ lib/arena_allocator_basic.h
@@ -50,53 +50,12 @@ concept CopyWithOffsetConstructable =
 template<typename T>
 concept ArenaAllocatorConstructable = PlainObject<T> || CopyWithOffsetConstructable<T>;
 
-template <typename T>
-class ArenaArrayView
-{
-private:
-    T* m_data;
-    std::size_t m_size;
-
-public:
-    // Container type definitions for compatibility with STL algorithms
-    using value_type = T;
-    using pointer = T*;
-    using const_pointer = const T*;
-    using reference = T&;
-    using const_reference = const T&;
-    using iterator = T*;
-    using const_iterator = const T*;
-
-    ArenaArrayView(): ArenaArrayView(nullptr, 0){}
-    ArenaArrayView(T* data, std::size_t size) : m_data(data), m_size(size) {}
-
-    // Observers and data access
-    T* data() noexcept { return m_data; }
-    const T* data() const noexcept { return m_data; }
-    std::size_t size() const noexcept { return m_size; }
-    bool empty() const noexcept { return m_size == 0; }
-
-    // Element access with bracket operator
-    T& operator[](std::size_t index) { return m_data[index]; }
-    const T& operator[](std::size_t index) const { return m_data[index]; }
-
-    // Iterator support
-    iterator begin() noexcept { return m_data; }
-    iterator end() noexcept { return m_data + m_size; }
-    const_iterator begin() const noexcept { return m_data; }
-    const_iterator end() const noexcept { return m_data + m_size; }
-    const_iterator cbegin() const noexcept { return m_data; }
-    const_iterator cend() const noexcept { return m_data + m_size; }
-};
-
 #if __cplusplus >= 202002L
 #include <span>
 template <typename T>
 using ArenaArrayResult = std::span<T>;
 #else
-// Fallback for older C++ standards
-template <typename T>
-using ArenaArrayResult = ArenaArrayView<T>;
+#error not supported
 #endif
 
 #endif // ARENA_ALLOCATOR_BASIC_H
